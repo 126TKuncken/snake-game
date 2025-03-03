161a9a23fd7f9cfa61ce10da89649faa433794de
Tyler Kuncken <126TKuncken@frhsd.com>
2025-03-03 14:04:32 -0500

    commit

--- js/snake.js
+++ js/snake.js
@@ -1,16 +1,17 @@
-export const SNAKE_SPEED = 2;
-const snakeBody = [
-{x: 10, y: 11},
-{x: 11, y: 11},
-];
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 5;
+const snakeBody = [{x: 11, y: 11},];
+let newSegments = 0;
 
 export function update() {
+    const inputDirection = getInputDirection();
 for (let i = snakeBody.length - 2; i >= 0; i--) {
 snakeBody[i + 1] = {...snakeBody[i]};
 }
 
-snakeBody[0].x += 0;
-snakeBody[0].y += 1;
+snakeBody[0].x += inputDirection.x;
+snakeBody[0].y += inputDirection.y;
 
 }
 
@@ -22,4 +23,18 @@ snakeElement.style.gridColumnStart = segment.x;
 snakeElement.classList.add('snake');
 gameBoard.appendChild(snakeElement);
 })
+}
+
+export function expandSnake(amount) {
+    newSegments += amount;
+}
+
+export function onSnake(position) {
+    return snakeBody.some(segment => {
+        return equalPositions(segment, position);
+    })
+}
+
+function equalPositions(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y;
 }
